8f275e18df35972458921f39ea02703d4ffd3041
oscargtg1t <106493026+oscargtg1t@users.noreply.github.com>
2023-06-08 00:19:56 -0600

    Update cargamento-search.php

--- cargamento-search.php
+++ cargamento-search.php
@@ -120,7 +120,7 @@
 				<div id="text">
 				<?php
 							if (isset($_GET['id_cargamento'])) {
-							$url = 'http://localhost:8012/appwebservice/cargamento.php?id_cargamento='. urlencode($_GET['id_cargamento']);
+							$url = 'http://143.110.209.5/cargamento.php?id_cargamento='. urlencode($_GET['id_cargamento']);
 							$cargamento = json_decode(file_get_contents($url));
 							if ($cargamento !== null) {
 								//echo '<h2>Cargamento Encontrado</h2>';
